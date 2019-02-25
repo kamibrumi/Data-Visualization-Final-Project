a9530b23a7fa13229bf6cd6c53ce7a4a3f3620cf
tgwiles <tgwiles@wpi.edu>
2019-02-25 14:49:14 -0500

    dynamically changes tree from user search

--- public/javascript/tree.js
+++ public/javascript/tree.js
@@ -1,46 +1,31 @@
-var treeData =
-    {
-        "name": "Top Level",
-        "children": [
-            {
-                "name": "Level 2: A",
-                "children": [
-                    { "name": "Son of A" },
-                    { "name": "Daughter of A" }
-                ]
-            },
-            { "name": "Level 2: B" }
-        ]
-    };
-
-function callGetSimilar(){
-    findSimilar("Muse", "Hysteria");
+function callGetSimilar() {
+    findSimilar(usrFindSimilar().artist, usrFindSimilar().track);
 }
 
 callGetSimilar();
 
-function drawData(data){
+function drawData(data) {
 
-    data = JSON.parse(data).children[0];
+    data = JSON.parse(data);
 
     // set the dimensions and margins of the diagram
     var margin = {top: 40, right: 90, bottom: 50, left: 90},
         width = 800 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-// declares a tree layout and assigns the size
+    // declares a tree layout and assigns the size
     var treemap = d3.tree()
         .size([width, height]);
 
-//  assigns the data to a hierarchy using parent-child relationships
+    //  assigns the data to a hierarchy using parent-child relationships
     var nodes = d3.hierarchy(data);
 
-// maps the node data to the tree layout
+    // maps the node data to the tree layout
     nodes = treemap(nodes);
 
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
     var svg = d3.select("body").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom),
@@ -48,38 +33,44 @@ function drawData(data){
             .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
 
-// adds the links between the nodes
+    // adds the links between the nodes
     var link = g.selectAll(".link")
-        .data( nodes.descendants().slice(1))
+        .data(nodes.descendants().slice(1))
         .enter().append("path")
         .attr("class", "link")
-        .attr("d", function(d) {
+        .attr("d", function (d) {
             return "M" + d.x + "," + d.y
                 + "C" + d.x + "," + (d.y + d.parent.y) / 2
-                + " " + d.parent.x + "," +  (d.y + d.parent.y) / 2
+                + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
                 + " " + d.parent.x + "," + d.parent.y;
         });
 
-// adds each node as a group
+    // adds each node as a group
     var node = g.selectAll(".node")
         .data(nodes.descendants())
         .enter().append("g")
-        .attr("class", function(d) {
+        .attr("class", function (d) {
             return "node" +
-                (d.children ? " node--internal" : " node--leaf"); })
-        .attr("transform", function(d) {
-            return "translate(" + d.x + "," + d.y + ")"; });
+                (d.children ? " node--internal" : " node--leaf");
+        })
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
 
-// adds the circle to the node
+    // adds the circle to the node
     node.append("circle")
         .attr("r", 10);
 
-// adds the text to the node
+    // adds the text to the node
     node.append("text")
         .attr("dy", ".35em")
-        .attr("y", function(d) { return d.children ? -20 : 20; })
+        .attr("y", function (d) {
+            return d.children ? -20 : 20;
+        })
         .style("text-anchor", "middle")
-        .text(function(d) { return d.data.name; });
+        .text(function (d) {
+            return d.data.name;
+        });
 }
 
 
